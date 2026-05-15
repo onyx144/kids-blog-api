427df9c3f417c670e6661f49e7763379f66b14bc
onyx144 <nesmeyanovnikita@gmail.com>
2026-05-16 00:10:23 +0300

    update: install

--- install.php
+++ install.php
@@ -165,7 +165,9 @@ try {
         DB_PASS,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::MYSQL_ATTR_INIT_COMMAND =>
+                'SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci',
         ]
     );
     
